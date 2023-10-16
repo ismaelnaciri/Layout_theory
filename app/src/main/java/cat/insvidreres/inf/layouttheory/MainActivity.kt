361dc5706b62c62d40c85c8e294cf0fab75671a1
ismaelnaciri <114733902+ismaelnaciri@users.noreply.github.com>
2023-10-16 14:08:54 +0200

    Initial commit

---
+++ app/src/main/java/cat/insvidreres/inf/layouttheory/MainActivity.kt
@@ -0,0 +1,11 @@
+package cat.insvidreres.inf.layouttheory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
